2b71130a5e21fc02b5aa085e4e0894eea002f8de
Anton <forvinwolves@gmail.com>
2016-06-03 14:06:25 +0300

    Натяжка

---
+++ wordpress/wp-content/themes/wp-komilfo-rostov/front-page.php
@@ -0,0 +1,161 @@
+<?php /* Template Name: Home Page Template */ get_header(); ?>
+
+    <div id="slideshow">
+  <div id="swappers"></div>
+  <div class="shadow-up"></div>
+</div>
+
+<div id="home-slider">
+  <div class="slider-container">
+    <div class="slider-wrapper theme-default">
+      <div id="slider" class="nivoSlider">
+
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/360a88f42424e06d03f11ba215c91a0913473a66.jpg" style="width: 1200px; visibility: hidden; display: inline;">
+
+
+                      <div id="caption0" class="nivo-html-caption">
+          <div class="nivo-cap-title"></div>
+          <div class="nivo-cap-descr"></div>
+        </div>
+
+
+
+    </div>
+  </div>
+</div>
+
+    <div id="main">
+      <div id="main">
+  <div class="main-container">
+    <div class="content content-full-width">
+      <div class="page type-page " style="text-align: center; font-family: Lobster;">
+        <div>
+                    <div class="hr_invisible "></div>
+
+          <div>
+            <h2 style=" font-size: 40px;">«Мода проходит, стиль остаётся»</h2>
+            <span style="font-size: 22px;">—&nbsp;Коко Шанель&nbsp;—</span>
+
+          </div>
+        </div>
+        <br>
+
+        <span class="see_more_span">
+          <a href="http://komilfo-rostov.com/pages/about">подробнее о салоне</a>
+        </span>
+
+        <br>
+        <div style="font-size: 22px; display: inline-block;position: relative; top: -8px;"> Мы в &nbsp; </div>
+
+        <a target="_blank" href="https://instagram.com/salon_krasoty_komilfo">
+        <img style="max-width: 125px;" src="<?php echo get_template_directory_uri(); ?>/img/instagramm.png"></a>
+                <br><br>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .see_more_span {
+    color: #322F20;
+    display: inline-block;
+    margin-bottom: 30px;
+    font-size: 18px;
+  }
+
+  .see_more_span a {
+    color: #322F20;
+    border-bottom: 2px dashed #322F20;
+  }
+
+  .see_more_span a:hover {
+    color: #AFA38A;
+    border-bottom: 2px dashed #AFA38A;
+  }
+</style>
+
+      <div id="footer">
+  <div class="main-container">
+    <div class="column one-third ">
+            <div class="widget widget_recent_entries lastnews">
+  <h2 class="widgettitle"><span>Вакансии</span></h2>
+
+  Во вопросам трудоустройства обращаться по телефону -&nbsp;8-928-988-89-29<br>Салону требуется мастер-универсал.&nbsp;<span></span>
+</div>    </div>
+
+
+    <div class="column one-third ">
+      <div class="widget widget_text"><h2 class="widgettitle"><span>Отзывы</span></h2>
+        <div class="textwidget">
+  <div class="testimonial-skin-carousel ">
+    <div class="jcarousel-container jcarousel-container-horizontal" style="position: relative; display: block;"><div class="jcarousel-clip jcarousel-clip-horizontal" style="position: relative;"><ul class="testimonial-carousel  jcarousel-list jcarousel-list-horizontal" style="overflow: hidden; position: relative; top: 0px; margin: 0px; padding: 0px; left: 0px; width: 1700px;">
+              <li class="jcarousel-item jcarousel-item-horizontal jcarousel-item-1 jcarousel-item-1-horizontal" jcarouselindex="1" style="float: left; list-style: none;">
+          <blockquote>
+            <p>
+              Косметология приятно удивила: в восторге от специалистов Наталии и Ирины (очень квалифицированные медики, и что особенно приятно, не "навязывают" процедур, которые действительно не нужны в моем случае). Девочки, спасибо за массаж "Бесхирургическая подтяжка" и элос эпиляцию. Вы профессионалы своего дела! Так держать              <cite> - Валерия</cite>
+            </p>
+          </blockquote>
+        </li>
+              <li class="jcarousel-item jcarousel-item-horizontal jcarousel-item-2 jcarousel-item-2-horizontal" jcarouselindex="2" style="float: left; list-style: none;">
+          <blockquote>
+            <p>
+              Салон один из лучших в городе! Супер обслуживание, супер персонал, супер цены!              <cite> - Григорий</cite>
+            </p>
+          </blockquote>
+        </li>
+              <li class="jcarousel-item jcarousel-item-horizontal jcarousel-item-3 jcarousel-item-3-horizontal" jcarouselindex="3" style="float: left; list-style: none;">
+          <blockquote>
+            <p>
+              Обожаю этот салон - каждый раз чувствую себя королевой! Очень добрые и отзывчивые мастера - обслуживание класс!              <cite> - Наташа</cite>
+            </p>
+          </blockquote>
+        </li>
+              <li class="jcarousel-item jcarousel-item-horizontal jcarousel-item-4 jcarousel-item-4-horizontal" jcarouselindex="4" style="float: left; list-style: none;">
+          <blockquote>
+            <p>
+              Отличный салон и мастера! Делала окрашивание волос и коррекцию бровей. Очень приятная атмосфера, администратор и мастера вежливые и приятные в общении люди. Обслуживание на 100%. Отдельное спасибо мастеру Юлии. Юлия, Ваша работа превзошла мои ожидания! Всем советую этот салон.              <cite> - Ольга</cite>
+            </p>
+          </blockquote>
+        </li>
+              <li class="jcarousel-item jcarousel-item-horizontal jcarousel-item-5 jcarousel-item-5-horizontal" jcarouselindex="5" style="float: left; list-style: none;">
+          <blockquote>
+            <p>
+              Очень внимательные сотрудники на ресепшн! Всегда чистота и уют,спасибо!              <cite> - Юлия</cite>
+            </p>
+          </blockquote>
+        </li>
+          </ul></div><div class="jcarousel-prev jcarousel-prev-horizontal jcarousel-prev-disabled jcarousel-prev-disabled-horizontal" style="display: block;" disabled="disabled"></div><div class="jcarousel-next jcarousel-next-horizontal" style="display: block;"></div></div>
+  </div>
+</div>
+
+      </div>
+      <div style="font-style: italic; padding-top: 10px;float: right;">
+        <a href="http://komilfo-rostov.com/guestbook">Все отзывы →</a>
+      </div>
+    </div>
+    <div class="column one-third last">
+      <div class="widget widget_text"><h2 class="widgettitle"><span>Контакты</span></h2>
+
+        <div class="textwidget">
+          <ul class="contact-details "><li><p>г. Ростов-на-Дону,&nbsp;ул.Вересаева 107а<br><a href="mailto:designthemes@somemail.com"></a></p></li><li><p>Телефон: (863)&nbsp;275-90-80,&nbsp;&nbsp;8-928-988-89-29&nbsp;</p></li><li><p>Режим работы: 10:00-21:00</p></li></ul>
+<a style="border-bottom: 2px dashed #AFA38A" href="http://komilfo-rostov.com/book">Записаться на прием</a>
+        </div>
+      </div>
+
+      <div id="text-9" class="widget widget_text">
+        <div class="textwidget"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="footer-bottom">
+
+</div>    </div>
+
+
+
+
+
+
+</body></html>
